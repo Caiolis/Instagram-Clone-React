f130328848cc8d34df11b0c2a8d42718abd025c1
Caio Carvalho <kaiogesteira@gmail.com>
2023-02-11 03:27:12 -0300

    Dinamic user name and user image

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,19 +1,50 @@
 import "./style.css";
+import { useState } from "react";
 
 export default function User() {
-    return (
-        <div class="profile-infos">
-                {/* <!-- Profile Image --> */}
-                <div class="profile-image">
-                    <img src="./assets/img/catanacomics 1.png" alt="Profile Pic" />
-                </div>
-                 {/* <!-- Profile Image End --> */}
+  // States
+  const [userName, setUserName] = useState("Guest");
+  const [userImage, setUserImage] = useState("./assets/img/catanacomics 1.png");
 
-                 {/* <!-- Profile Desc --> */}
-                 <div class="profile-desc">
-                    <p class="profile-name">catanacomics<br /><span class="sub-name">Catarina</span></p>
-                 </div>
-                 {/* <!-- Profile Desc End--> */}
-            </div>
-    );
+  // Changes the name of the user when the pencil is clicked.
+  function changeUserName() {
+    let name = prompt("Digite o nome do usuário");
+
+    // If the name is empty, it returns nothing, if it is not, it updates the name.
+    if (!name) {
+      return;
+    } else {
+      setUserName(name);
+    }
+  }
+
+  // Changes the image of the user when the image is clicked.
+  function changeUserImage() {
+    let image = prompt("Enter your image url");
+
+    // If the image is empty, it returns nothing, if it is not, it updates the image.
+    if (!image) {
+      return;
+    } else {
+      setUserImage(image);
+    }
+  }
+
+  return (
+    <div class="profile-infos">
+      {/* <!-- Profile Image --> */}
+      <div class="profile-image user-image" onClick={changeUserImage}>
+        <img src={userImage} alt="Profile Pic" />
+      </div>
+      {/* <!-- Profile Image End --> */}
+
+      {/* <!-- Profile Desc --> */}
+      <div class="profile-desc" onClick={changeUserName}>
+        <p class="profile-name">
+          {userName} <ion-icon name="pencil-outline"></ion-icon>
+        </p>
+      </div>
+      {/* <!-- Profile Desc End--> */}
+    </div>
+  );
 }
