39858d9e9d5e7149b14017ae03f81527d30f7f13
Caio Carvalho <kaiogesteira@gmail.com>
2023-02-13 16:21:23 -0300

    Stories being generated dinamically

--- src/Components/Stories/Stories.js
+++ src/Components/Stories/Stories.js
@@ -1,5 +1,55 @@
 import "./style.css"
 
+// Stories Informations 
+const Itens = [
+    {
+        img: "./assets/img/9gag 1.png",
+        user: "9gag"
+    },
+    {
+        img: "./assets/img/meowed 1.png",
+        user: "meowed"
+    },
+    {
+        img: "./assets/img/nathanwpylestrangeplanet 1.png",
+        user: "nathanwpyle..."
+    },
+    {
+        img: "./assets/img/wawawiwacomicsa 1.png",
+        user: "wawawiwac..."
+    },
+    {
+        img: "./assets/img/respondeai 1.png",
+        user: "respondeai"
+    },
+    {
+        img: "./assets/img/9gag 1.png",
+        user: "9gag"
+    },
+    {
+        img: "./assets/img/meowed 1.png",
+        user: "filomoderna"
+    },
+    {
+        img: "./assets/img/memeriagourmet 1.png",
+        user: "memeriago"
+    }
+];
+
+// Stories Templates
+function StoriesComponents(props) {
+    return (
+        <div class="stories-account">
+            <div class="stories-pic">
+                <img src={props.img} alt="stories pic" />
+            </div>
+            <span>{props.user}</span>
+        </div>
+    )
+}
+
+
+// Stories being created by a map
 export default function Stories() {
     return (
        
@@ -8,54 +58,7 @@ export default function Stories() {
                     <img src="./assets/img/Vector.png" class="shadow" />
                 </div>
                 {/* <!-- Stories account --> */}
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/9gag 1.png" alt="stories pic" />
-                    </div>
-                    <span>9gag</span>
-                </div>
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/meowed 1.png" alt="stories pic" />
-                    </div>
-                    <span>meowed</span>
-                </div>
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/barked 1.png" alt="stories pic" />
-                    </div>
-                    <span>barked</span>
-                </div>
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/nathanwpylestrangeplanet 1.png" alt="stories pic" />
-                    </div>
-                    <span>nathanwpyle...</span>
-                </div>
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/wawawiwacomicsa 1.png" alt="stories pic" />
-                    </div>
-                    <span>wawawiwac...</span>
-                </div>
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/respondeai 1.png" alt="stories pic"/>
-                    </div>
-                    <span>respondeai</span>
-                </div>
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/meowed 1.png" alt="stories pic" />
-                    </div>
-                    <span>filomoderna</span>
-                </div>
-                <div class="stories-account">
-                    <div class="stories-pic">
-                        <img src="./assets/img/memeriagourmet 1.png" alt="stories pic" />
-                    </div>
-                    <span>memeriago.</span>
-                </div>
+                {Itens.map(item => (<StoriesComponents img={item.img} user={item.user}/>))}
                 {/* <!-- Stories account End--> */}
         </div>
         
