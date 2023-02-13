dae407c5e04d54cd891d8b74440ea4e9883b7ce3
Caio Carvalho <kaiogesteira@gmail.com>
2023-02-13 16:11:11 -0300

    Like system

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,206 +1,139 @@
 import "./style.css";
+import { useState } from "react";
 
+// Posts Informations
+const Itens = [
+    {
+        userImg: "./assets/img/delish-profile-pic.jpg",
+        userName: "Delish",
+        postImage: "./assets/img/gato-telefone 1.png",
+        commentImage: "./assets/img/respondeai 2.png",
+        commentUser: "respondeai"
+    },
+    {
+        userImg: "./assets/img/barked 2.png",
+        userName: "barked",
+        postImage: "./assets/img/dog 1.png",
+        commentImage: "./assets/img/adorableanimals 2.png",
+        commentUser: "adorable_animals"
+    },
+    {
+        userImg: "./assets/img/deschamps.jpg",
+        userName: "filipedeschamps",
+        postImage: "./assets/img/deschamps-post.jpg",
+        commentImage: "./assets/img/adorableanimals 2.png",
+        commentUser: "adorable_animals"
+    }
+];
+
+// Posts Template
+function MenuPost(props) {
+
+    // Like informations
+    const [icon, setIcon] = useState(<ion-icon name="heart-outline"></ion-icon>);
+    const [isLiked, setIsLiked] = useState(false);
+
+    // Likes the post, if the user clicked on the image it will only like, if the user clicked on the heart
+    // It will like and dislike the post
+    function like(clickedImage=false) {
+        if(clickedImage) {
+            setIcon(<ion-icon name="heart" color="danger"></ion-icon>);
+            setIsLiked(true);
+        } else {
+            if(!isLiked) {
+                setIcon(<ion-icon name="heart" color="danger"></ion-icon>);
+                setIsLiked(true);
+            } else {
+                setIcon(<ion-icon name="heart-outline"></ion-icon>);
+                setIsLiked(false);
+            }
+        }
+    } 
+
+    return (
+        <div class="posts-wrapper">
+        {/* <!-- Upper Post Bar --> */}
+        <div class="upper-post-bar">
+            {/* <!-- Profile Desc --> */}
+            <div class="profile-desc">
+                <div class="image-wrapper">
+                    <img src={props.userImg} alt={props.userName} />
+                </div>
+
+                <div class="name-wrapper">
+                    <a href="#">{props.userName}</a>
+                </div>
+            </div>
+            {/* <!-- Profile Desc End --> */}
+
+            {/* <!-- Options --> */}
+            <div class="options">
+                <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+            </div>
+            {/* <!-- Options End--> */}
+        </div>
+        {/* <!-- Upper Post Bar End --> */}
+
+        {/* <!-- Post Content --> */}
+        <div class="post-content">
+            <img src={props.postImage} alt="Foto do post" onClick={() => like(true)}/>
+        </div>
+        {/* <!-- Post Content End --> */}
+
+        {/* <!-- Bottom Post Infos --> */}
+        <div class="bottom-post-infos">
+            {/* <!-- Icons Post Infos --> */}
+            <div class="icons-post-infos">
+                {/* <!-- Left post Infos --> */}
+                <div class="left-post-infos">
+                    <span onClick={() => like()}>
+                        {icon}
+                    </span>
+                    <ion-icon name="chatbubble-outline" class="middle"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                </div>
+                {/* <!-- Left post Infos End--> */}
+
+                {/* <!-- Right post Info --> */}
+                <div class="right-post-info">
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                </div>
+                {/* <!-- Right post Info End --> */}
+            </div>
+            {/* <!-- Icons Post Infos End --> */}
+
+            {/* <!-- Liked by infos --> */}
+            <div class="liked-by-infos">
+                {/* <!-- Liked By Image --> */}
+                <div class="liked-by-image">
+                    <img src={props.commentImage} alt={props.commentUser} />
+                </div>
+                {/* <!-- Liked By Image End -->  */}
+
+                <div class="liked-by-desc">
+                    <span>Curtido por <strong>{props.commentUser}</strong> e <strong>outras 101.523 pessoas</strong></span>
+                </div>
+            </div>
+            {/* <!-- Liked by infos End--> */}
+        </div>
+        {/* <!-- Bottom Post Infos End--> */}
+        </div>
+    )
+}
+
+// Generates the posts with a map
 export default function Post() {
     return (
-        <div>
-            <div class="posts-wrapper">
-            {/* <!-- Upper Post Bar --> */}
-            <div class="upper-post-bar">
-                {/* <!-- Profile Desc --> */}
-                <div class="profile-desc">
-                    <div class="image-wrapper">
-                        <img src="./assets/img/delish-profile-pic.jpg" alt="delish" />
-                    </div>
-
-                    <div class="name-wrapper">
-                        <a href="#">delish</a>
-                    </div>
-                </div>
-                {/* <!-- Profile Desc End --> */}
-
-                {/* <!-- Options --> */}
-                <div class="options">
-                    <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
-                </div>
-                {/* <!-- Options End--> */}
-            </div>
-            {/* <!-- Upper Post Bar End --> */}
-
-            {/* <!-- Post Content --> */}
-            <div class="post-content">
-                <img src="./assets/img/gato-telefone 1.png" alt="Foto do post" />
-            </div>
-            {/* <!-- Post Content End --> */}
-
-            {/* <!-- Bottom Post Infos --> */}
-            <div class="bottom-post-infos">
-                {/* <!-- Icons Post Infos --> */}
-                <div class="icons-post-infos">
-                    {/* <!-- Left post Infos --> */}
-                    <div class="left-post-infos">
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline" class="middle"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    {/* <!-- Left post Infos End--> */}
-
-                    {/* <!-- Right post Info --> */}
-                    <div class="right-post-info">
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                    {/* <!-- Right post Info End --> */}
-                </div>
-                {/* <!-- Icons Post Infos End --> */}
-
-                {/* <!-- Liked by infos --> */}
-                <div class="liked-by-infos">
-                    {/* <!-- Liked By Image --> */}
-                    <div class="liked-by-image">
-                        <img src="./assets/img/respondeai 2.png" alt="Responde aí" />
-                    </div>
-                    {/* <!-- Liked By Image End -->  */}
-
-                    <div class="liked-by-desc">
-                        <span>Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong></span>
-                    </div>
-                </div>
-                {/* <!-- Liked by infos End--> */}
-            </div>
-            {/* <!-- Bottom Post Infos End--> */}
+        <div class="posts">
+        {
+        Itens.map((item) => 
+        <MenuPost 
+            userImg={item.userImg} 
+            userName={item.userName} 
+            postImage={item.postImage}
+            commentImage={item.commentImage}
+            commentUser={item.commentUser} 
+        />)}
         </div>
-
-        <div class="posts-wrapper post-gap">
-            {/* <!-- Upper Post Bar --> */}
-            <div class="upper-post-bar">
-                {/* <!-- Profile Desc --> */}
-                <div class="profile-desc">
-                    <div class="image-wrapper">
-                        <img src="./assets/img/barked 2.png" alt="Barked" />
-                    </div>
-
-                    <div class="name-wrapper">
-                        <a href="#">barked</a>
-                    </div>
-                </div>
-                {/* <!-- Profile Desc End --> */}
-
-                {/* <!-- Options --> */}
-                <div class="options">
-                    <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
-                </div>
-                {/* <!-- Options End--> */}
-            </div>
-            {/* <!-- Upper Post Bar End --> */}
-
-            {/* <!-- Post Content --> */}
-            <div class="post-content">
-                <img src="./assets/img/dog 1.png" alt="Foto do post" />
-            </div>
-            {/* <!-- Post Content End --> */}
-
-            {/* <!-- Bottom Post Infos --> */}
-            <div class="bottom-post-infos">
-                {/* <!-- Icons Post Infos --> */}
-                <div class="icons-post-infos">
-                    {/* <!-- Left post Infos --> */}
-                    <div class="left-post-infos">
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline" class="middle"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    {/* <!-- Left post Infos End--> */}
-
-                    {/* <!-- Right post Info --> */}
-                    <div class="right-post-info">
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                    {/* <!-- Right post Info End --> */}
-                </div>
-                {/* <!-- Icons Post Infos End --> */}
-
-                {/* <!-- Liked by infos --> */}
-                <div class="liked-by-infos">
-                    {/* <!-- Liked By Image --> */}
-                    <div class="liked-by-image">
-                        <img src="./assets/img/adorableanimals 2.png" alt="Adorable Animals" />
-                    </div>
-                    {/* <!-- Liked By Image End -->  */}
-
-                    <div class="liked-by-desc">
-                        <span>Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong></span>
-                    </div>
-                </div>
-                {/* <!-- Liked by infos End--> */}
-            </div>
-            {/* <!-- Bottom Post Infos End--> */}
-        </div>
-
-        <div class="posts-wrapper post-gap">
-            {/* <!-- Upper Post Bar --> */}
-            <div class="upper-post-bar">
-                {/* <!-- Profile Desc --> */}
-                <div class="profile-desc">
-                    <div class="image-wrapper">
-                        <img src="./assets/img/deschamps.jpg" alt="deschamps" />
-                    </div>
-
-                    <div class="name-wrapper">
-                        <a href="#">filipedeschamps</a>
-                    </div>
-                </div>
-                {/* <!-- Profile Desc End --> */}
-
-                {/* <!-- Options --> */}
-                <div class="options">
-                    <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
-                </div>
-                {/* <!-- Options End--> */}
-            </div>
-            {/* <!-- Upper Post Bar End --> */}
-
-            {/* <!-- Post Content --> */}
-            <div class="post-content">
-                <img src="./assets/img/deschamps-post.jpg" alt="Foto do post" />
-            </div>
-            {/* <!-- Post Content End --> */}
-
-            {/* <!-- Bottom Post Infos --> */}
-            <div class="bottom-post-infos">
-                {/* <!-- Icons Post Infos --> */}
-                <div class="icons-post-infos">
-                    {/* <!-- Left post Infos --> */}
-                    <div class="left-post-infos">
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline" class="middle"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    {/* <!-- Left post Infos End--> */}
-
-                    {/* <!-- Right post Info --> */}
-                    <div class="right-post-info">
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                    {/* <!-- Right post Info End --> */}
-                </div>
-                {/* <!-- Icons Post Infos End --> */}
-
-                {/* <!-- Liked by infos --> */}
-                <div class="liked-by-infos">
-                    {/* <!-- Liked By Image --> */}
-                    <div class="liked-by-image">
-                        <img src="./assets/img/adorableanimals 2.png" alt="Adorable Animals" />
-                    </div>
-                    {/* <!-- Liked By Image End -->  */}
-
-                    <div class="liked-by-desc">
-                        <span>Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong></span>
-                    </div>
-                </div>
-                {/* <!-- Liked by infos End--> */}
-            </div>
-            {/* <!-- Bottom Post Infos End--> */}
-        </div>
-        </div>
-    
     )
 }
