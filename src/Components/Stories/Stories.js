c7e544cddb88ed55850b41c247c47198bd789d3b
Caio Carvalho <kaiogesteira@gmail.com>
2023-02-13 16:37:46 -0300

    Suggestions generated dinamically

--- src/Components/Stories/Stories.js
+++ src/Components/Stories/Stories.js
@@ -1,66 +1,65 @@
-import "./style.css"
+import "./style.css";
 
-// Stories Informations 
+// Stories Informations
 const Itens = [
-    {
-        img: "./assets/img/9gag 1.png",
-        user: "9gag"
-    },
-    {
-        img: "./assets/img/meowed 1.png",
-        user: "meowed"
-    },
-    {
-        img: "./assets/img/nathanwpylestrangeplanet 1.png",
-        user: "nathanwpyle..."
-    },
-    {
-        img: "./assets/img/wawawiwacomicsa 1.png",
-        user: "wawawiwac..."
-    },
-    {
-        img: "./assets/img/respondeai 1.png",
-        user: "respondeai"
-    },
-    {
-        img: "./assets/img/9gag 1.png",
-        user: "9gag"
-    },
-    {
-        img: "./assets/img/meowed 1.png",
-        user: "filomoderna"
-    },
-    {
-        img: "./assets/img/memeriagourmet 1.png",
-        user: "memeriago"
-    }
+  {
+    img: "./assets/img/9gag 1.png",
+    user: "9gag",
+  },
+  {
+    img: "./assets/img/meowed 1.png",
+    user: "meowed",
+  },
+  {
+    img: "./assets/img/nathanwpylestrangeplanet 1.png",
+    user: "nathanwpyle...",
+  },
+  {
+    img: "./assets/img/wawawiwacomicsa 1.png",
+    user: "wawawiwac...",
+  },
+  {
+    img: "./assets/img/respondeai 1.png",
+    user: "respondeai",
+  },
+  {
+    img: "./assets/img/9gag 1.png",
+    user: "9gag",
+  },
+  {
+    img: "./assets/img/meowed 1.png",
+    user: "filomoderna",
+  },
+  {
+    img: "./assets/img/memeriagourmet 1.png",
+    user: "memeriago",
+  },
 ];
 
 // Stories Templates
 function StoriesComponents(props) {
-    return (
-        <div class="stories-account">
-            <div class="stories-pic">
-                <img src={props.img} alt="stories pic" />
-            </div>
-            <span>{props.user}</span>
-        </div>
-    )
+  return (
+    <div class="stories-account">
+      <div class="stories-pic">
+        <img src={props.img} alt="stories pic" />
+      </div>
+      <span>{props.user}</span>
+    </div>
+  );
 }
-
 
 // Stories being created by a map
 export default function Stories() {
-    return (
-       
-        <div class="stories-bar">
-                <div class="icon-stories">
-                    <img src="./assets/img/Vector.png" class="shadow" />
-                </div>
-                {/* <!-- Stories account --> */}
-                {Itens.map(item => (<StoriesComponents img={item.img} user={item.user}/>))}
-                {/* <!-- Stories account End--> */}
-        </div>
-        
-    )
+  return (
+    <div class="stories-bar">
+      <div class="icon-stories">
+        <img src="./assets/img/Vector.png" class="shadow" />
+      </div>
+      {/* <!-- Stories account --> */}
+      {Itens.map((item) => (
+        <StoriesComponents img={item.img} user={item.user} />
+      ))}
+      {/* <!-- Stories account End--> */}
+    </div>
+  );
 }
--- src/Components/Sugestions/Sugestions.js
+++ src/Components/Sugestions/Sugestions.js
@@ -1,155 +1,94 @@
 import "./style.css";
 
+// Sugestions Information
+const Itens = [
+  {
+    img: "./assets/img/badvibesmemes 1.png",
+    user: "bad.vibes.memes",
+    status: "Segue você",
+  },
+  {
+    img: "./assets/img/chibirdart 1.png",
+    user: "chibirdart",
+    status: "Segue você",
+  },
+  {
+    img: "./assets/img/razoesparaacreditar 1.png",
+    user: "razoesparaacreditar",
+    status: "Novo no Instagram",
+  },
+  {
+    img: "./assets/img/adorableanimals 2.png",
+    user: "adorable_animals",
+    status: "Segue você",
+  },
+  {
+    img: "./assets/img/smallcutecats 1.png",
+    user: "smallcutecats",
+    status: "Segue você",
+  },
+];
+
+function SugestionsTemplate(props) {
+  return (
+    <div class="profile-infos profile-gap">
+      {/* <!-- Profile-infos-right --> */}
+      <div class="profile-infos-right">
+        {/* <!-- Profile Image --> */}
+        <div class="profile-image">
+          <img
+            class="card-image"
+            src={props.img}
+            alt="Profile Pic"
+          />
+        </div>
+        {/* <!-- Profile Image End --> */}
+
+        {/* <!-- Profile Desc --> */}
+        <div class="profile-desc">
+          <p class="profile-name">
+            {props.user}
+            <br />
+            <span class="sub-name card-follow">{props.status}</span>
+          </p>
+        </div>
+        {/* <!-- Profile Desc End--> */}
+      </div>
+      {/* <!-- Profile-infos-right End--> */}
+
+      {/* <!-- Profile infos left --> */}
+      <div class="profile-infos-left">
+        <a href="#">Seguir</a>
+      </div>
+      {/* <!-- Profile infos left End --> */}
+    </div>
+  );
+}
+
 export default function Sugestions() {
-    return (
-        <div class="follow">
-                {/* <!-- You may like it --> */}
-                <div class="you-may-like-it">
-                    {/* <!-- You may Like it Right --> */}
-                    <div class="may-like-it-right">
-                        <span>Sugestões para você</span>
-                    </div>
-                    {/* <!-- You may Like it Right End--> */}
+  return (
+    <div class="follow">
+      {/* <!-- You may like it --> */}
+      <div class="you-may-like-it">
+        {/* <!-- You may Like it Right --> */}
+        <div class="may-like-it-right">
+          <span>Sugestões para você</span>
+        </div>
+        {/* <!-- You may Like it Right End--> */}
 
-                    {/* <!-- You May like it Left --> */}
-                    <div class="may-like-it-left">
-                        <a href="#">Ver tudo</a>
-                    </div>
-                    {/* <!-- You May like it Left End--> */}
+        {/* <!-- You May like it Left --> */}
+        <div class="may-like-it-left">
+          <a href="#">Ver tudo</a>
+        </div>
+        {/* <!-- You May like it Left End--> */}
+      </div>
+      {/* <!-- You may like it end--> */}
 
-                </div>
-                {/* <!-- You may like it end--> */}
-
-                {/* <!-- Cards Wrapper --> */}
-                <div class="cards-wrapper">
-                    <div class="profile-infos">
-                        {/* <!-- Profile-infos-right --> */}
-                        <div class="profile-infos-right">
-                            {/* <!-- Profile Image --> */}
-                            <div class="profile-image">
-                                <img class='card-image' src="./assets/img/badvibesmemes 1.png" alt="Profile Pic" />
-                            </div>
-                            {/* <!-- Profile Image End --> */}
-            
-                            {/* <!-- Profile Desc --> */}
-                            <div class="profile-desc">
-                                <p class="profile-name">bad.vibes.memes<br/><span class="sub-name card-follow">Segue você</span></p>
-                            </div>
-                            {/* <!-- Profile Desc End--> */}
-                        </div>
-                        {/* <!-- Profile-infos-right End--> */}
-
-
-                        {/* <!-- Profile infos left --> */}
-                        <div class="profile-infos-left">
-                            <a href="#">Seguir</a>
-                        </div>
-                        {/* <!-- Profile infos left End --> */}
-
-                    </div>
-                    <div class="profile-infos profile-gap">
-                        {/* <!-- Profile-infos-right --> */}
-                        <div class="profile-infos-right">
-                            {/* <!-- Profile Image --> */}
-                            <div class="profile-image">
-                                <img class='card-image' src="./assets/img/chibirdart 1.png" alt="Profile Pic" />
-                            </div>
-                            {/* <!-- Profile Image End --> */}
-            
-                            {/* <!-- Profile Desc --> */}
-                            <div class="profile-desc">
-                                <p class="profile-name">chibirdart<br/><span class="sub-name card-follow">Segue você</span></p>
-                            </div>
-                            {/* <!-- Profile Desc End--> */}
-                        </div>
-                        {/* <!-- Profile-infos-right End--> */}
-
-
-                        {/* <!-- Profile infos left --> */}
-                        <div class="profile-infos-left">
-                            <a href="#">Seguir</a>
-                        </div>
-                        {/* <!-- Profile infos left End --> */}
-
-                    </div>
-                    <div class="profile-infos profile-gap">
-                        {/* <!-- Profile-infos-right --> */}
-                        <div class="profile-infos-right">
-                            {/* <!-- Profile Image --> */}
-                            <div class="profile-image">
-                                <img class='card-image' src="./assets/img/razoesparaacreditar 1.png" alt="Profile Pic" />
-                            </div>
-                            {/* <!-- Profile Image End --> */}
-            
-                            {/* <!-- Profile Desc --> */}
-                            <div class="profile-desc">
-                                <p class="profile-name">razoesparaacreditar<br/><span class="sub-name card-follow">Novo no Instagram</span></p>
-                            </div>
-                            {/* <!-- Profile Desc End--> */}
-                        </div>
-                        {/* <!-- Profile-infos-right End--> */}
-
-
-                        {/* <!-- Profile infos left --> */}
-                        <div class="profile-infos-left">
-                            <a href="#">Seguir</a>
-                        </div>
-                        {/* <!-- Profile infos left End --> */}
-
-                    </div>
-                    <div class="profile-infos profile-gap">
-                        {/* <!-- Profile-infos-right --> */}
-                        <div class="profile-infos-right">
-                            {/* <!-- Profile Image --> */}
-                            <div class="profile-image">
-                                <img class='card-image' src="./assets/img/adorableanimals 2.png" alt="Profile Pic" />
-                            </div>
-                            {/* <!-- Profile Image End --> */}
-            
-                            {/* <!-- Profile Desc --> */}
-                            <div class="profile-desc">
-                                <p class="profile-name">adorable_animals<br/><span class="sub-name card-follow">Segue você</span></p>
-                            </div>
-                            {/* <!-- Profile Desc End--> */}
-                        </div>
-                        {/* <!-- Profile-infos-right End--> */}
-
-
-                        {/* <!-- Profile infos left --> */}
-                        <div class="profile-infos-left">
-                            <a href="#">Seguir</a>
-                        </div>
-                        {/* <!-- Profile infos left End --> */}
-
-                    </div>
-                    <div class="profile-infos profile-gap">
-                        {/* <!-- Profile-infos-right --> */}
-                        <div class="profile-infos-right">
-                            {/* <!-- Profile Image --> */}
-                            <div class="profile-image">
-                                <img class='card-image' src="./assets/img/smallcutecats 1.png" alt="Profile Pic" />
-                            </div>
-                            {/* <!-- Profile Image End --> */}
-            
-                            {/* <!-- Profile Desc --> */}
-                            <div class="profile-desc">
-                                <p class="profile-name">smallcutecats<br/><span class="sub-name card-follow">Segue você</span></p>
-                            </div>
-                            {/* <!-- Profile Desc End--> */}
-                        </div>
-                        {/* <!-- Profile-infos-right End--> */}
-
-
-                        {/* <!-- Profile infos left --> */}
-                        <div class="profile-infos-left">
-                            <a href="#">Seguir</a>
-                        </div>
-                        {/* <!-- Profile infos left End --> */}
-
-                    </div>
-                    
-                </div>
-                {/* <!-- Cards Wrapper End --> */}
-            </div>
-    )
+      {/* <!-- Cards Wrapper --> */}
+      <div class="cards-wrapper">
+        {Itens.map(item => (<SugestionsTemplate img={item.img} user={item.user} status={item.status}/>))}
+      </div>
+      {/* <!-- Cards Wrapper End --> */}
+    </div>
+  );
 }
